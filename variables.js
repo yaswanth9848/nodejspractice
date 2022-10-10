57c037149341cb707f69faf622bfbc6fe636c5f1
yaswanth9848 <yashwanth.sai.pavuluri@gmail.com>
2022-10-10 20:05:44 +0530

    Differences in variables

---
+++ variables.js
@@ -0,0 +1,13 @@
+var a = 10;
+var a = 20;    // in var any no.of keywords will be given,but last keyword will be assigned to a variable
+console.log(a);
+ 
+let b = 10;
+let b = 20; // in let it doesn't accept the second time keyword assigning 
+console.log(b);
+//error
+
+const  c = 10;
+const  c = 20;
+console.log(c); // const is used for constant values and it cannot assign second time
+//error
