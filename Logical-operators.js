2a752b94e71bd22b8a41073729aa05d5a745f72d
yaswanth9848 <yashwanth.sai.pavuluri@gmail.com>
2022-10-10 22:46:00 +0530

    logicaloperator

---
+++ Logical-operators.js
@@ -0,0 +1,19 @@
+var num1= 10;
+var num2 = 20;
+var num3 = 30;
+var num4  = 40;
+if(num1<num2 && num2<num3 )  // && is used to check both the conditions are true
+{
+    console.log("num3 is greater");
+}
+else(num3<num4 || num4<num1)  //  || is used to check if either one statement is true
+{
+    console.log("num 4 is greater");
+} 
+
+var h = 10;
+var i = 20;
+if(h!=i)            // not equal
+{
+    console.log("Both are not same");
+}
