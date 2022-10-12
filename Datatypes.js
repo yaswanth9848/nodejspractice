382419b4512a8d9e766212380941aaaf4a05dedb
yaswanth9848 <yashwanth.sai.pavuluri@gmail.com>
2022-10-12 13:27:20 +0530

    Datatypes

---
+++ Datatypes.js
@@ -0,0 +1,27 @@
+var a = 10;    //number
+var b = 3.14;  //number
+var c = "30" //string
+var d = "JavaScript"
+var e = "good Morning"
+console.log(a + b + c+d+e)
+console.log (typeof a ) 
+console.log (typeof b )
+console.log (typeof c )
+console.log (typeof d )
+console.log (typeof e )
+
+const y = 10000000
+console.log(typeof y)
+
+let h = 6;
+let i = 7;
+console.log((h+2*i));
+
+j = 10;
+k = 20;
+bool=j<k    //bool is used to check the statement either true or false
+console.log(bool);
+
+let user = 3;
+console.log(user);
+
