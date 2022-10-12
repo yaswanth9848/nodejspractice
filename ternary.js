a2081add634b9a53d34d85ddb159649c1111da9e
yaswanth9848 <yashwanth.sai.pavuluri@gmail.com>
2022-10-12 16:57:19 +0530

    ternaryoperator

---
+++ ternary.js
@@ -0,0 +1,12 @@
+var a = 10;
+var b = 20;
+var result ; // ternary operator is used to check true or false
+result = (a<b ? "true" : "false") //(conition? "true":"false")
+{
+    console.log(result);
+}
+
+var a = 1000;
+var b = 200;
+result = (a<b? "true" : "false")
+console.log(result);
