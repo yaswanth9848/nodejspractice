45343eb367dd6bed98186298ffcc5cfe2e1f23e0
yaswanth9848 <yashwanth.sai.pavuluri@gmail.com>
2022-10-18 18:12:31 +0530

    loops

---
+++ Switch.js
@@ -0,0 +1,51 @@
+const a = "Yaswanth"
+switch(a)
+{
+    case  'Roshan' :
+    {
+    console.log("Mismatch");
+    }
+    break;
+    case  'Akshaya':
+    {
+        console.log("Mismatch");
+    }
+    break;
+    case   "Sakshi" :
+    {
+        console.log("Mismatch");
+    }
+    break;
+    case   "Arun":
+    {
+        console.log("Mismatch");
+    }
+    break;
+    case  "faiz" :
+    {
+        console.log("Mismatch");
+    }
+    break;
+
+default :
+console.log("No Name is Matched");
+}
+
+switch ("jan")
+{
+    case  'jan':
+        {
+      console.log("31 days");
+        } 
+     break;
+    case 'feb':
+        console.log("28 days");
+    break;
+    case 'mar' :
+        console.log("30 days");
+        break;
+      default:
+        console.log("No month is matched");
+}
+
+
---
+++ forloop.js
@@ -0,0 +1,12 @@
+// for loop
+const a = 5;
+for(i = 0;i<a;i++)
+{
+console.log(i);
+}
+const  b = [1,2,3,4,5];
+for( i=0;i<b.length;i++)
+{
+    b[i] = b[i]+2;   
+}
+console.log(b);
