11ea3169efa0b4fdc8c0aab192b63b693fe3cfc1
yaswanth9848 <yashwanth.sai.pavuluri@gmail.com>
2022-10-18 18:10:05 +0530

    if statement

---
+++ if.js
@@ -0,0 +1,68 @@
+//Using if else
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
+if(a==b)
+{
+    console.log("true");
+}
+else
+{
+    console.log("false");
+}
+
+// using if else if ladder
+
+const month = ["january","february","march","april","may","june"];
+ if (month == "july")
+ {
+    console.log("mis matched");
+ }
+ else if(month == "august")
+ {
+    console.log("mis matched");
+ }
+ else if (month == "september")
+ {
+    console.log("mis matched");
+ }
+ else if (month == "october")
+ {
+    console.log("mis matched");
+ }
+ else if (month == "november")
+ {
+    console.log("mis matched");
+ }
+ else if (month == "december")
+ {
+    console.log("mis matched");
+ }
+ else
+ {
+    console.log("nothing is matched");
+ }
+
+ // 
+ const Apps = ["facebook","Youtube","whatsapp","telegram","instagram"];
+if(Apps == "My jio")
+{
+   console.log("it is not a social media app");
+}
+else if (Apps == "Airtel")
+{
+   console.log("it is not a social media app");
+}
+else if (Apps == "Vi")
+{
+   console.log("it is not a social media app");
+}
+else if (Apps == "Bsnl")
+{
+   console.log("it is not a social media app");
+}
+else
+{
+   console.log("Please enter a social media application");
+}
+
+
