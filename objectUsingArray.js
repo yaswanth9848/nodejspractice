92a5d7668dfb4abb499a6ff91f49e9b2b8511cbb
yaswanth9848 <yashwanth.sai.pavuluri@gmail.com>
2022-10-20 20:04:53 +0530

    Obects using arrays

---
+++ objectUsingArray.js
@@ -0,0 +1,9 @@
+let mobiles = [
+
+    {model:"redmi",country:"china"},
+    {model:"realme",country:"china"},
+    {model:"samsung",country:"south korea"},
+    {model:"oppo",country:"china"},
+    {model:"moto",country:"USA"},
+]
+  console.log(mobiles);
