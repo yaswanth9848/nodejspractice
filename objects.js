0b16ef77351334ee435106196030340271d6a9b9
yaswanth9848 <yashwanth.sai.pavuluri@gmail.com>
2022-10-20 20:02:52 +0530

    5programs on objects

---
+++ objects.js
@@ -0,0 +1,51 @@
+//programs on objects
+//1.students
+const students = {
+    name:"ram",
+    age:15,
+    class:10,
+    state:"Ap"
+
+}
+console.log(students);
+
+//2.car
+const car = {
+    name:"punch",
+    company:"TATA",
+    fueltype:"Petrol",
+    engine:"10000cc"
+
+
+}
+console.log(car);
+//3.bikes
+const bikes ={
+    1:"Royal enfield",
+    2:"Unicorn",
+    3:"shine",
+    4:"Glamour",
+    5:"star city"
+    
+}
+console.log(bikes);
+
+//4.mobiles
+const mobiles=
+{
+    1:"Apple",
+    2:"Samsung",
+    3:"Oneplus",
+    4:"Redmi",
+    5:"Realme"
+}
+console.log(mobiles);
+//5.applications
+const apps = {
+    1:"phonepay",
+    2:"Gpay",
+    3:"whatsapp",
+    4:"Telegram",
+    5:"Facebook"
+}
+console.log(apps);
