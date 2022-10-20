a67bfd173cc2d8d2861063105515874afe651a86
yaswanth9848 <yashwanth.sai.pavuluri@gmail.com>
2022-10-20 20:08:15 +0530

    objects using functions

---
+++ objectUsingFunctions.js
@@ -0,0 +1,22 @@
+//using functions in objects
+const person = 
+{
+    name:"ashok",
+    age:20,
+    profession:"student",
+    course:"b.tech",
+    college:"jntuk",
+    sem:1,
+    subjects:function sub()
+    {
+       english
+       maths
+       physics
+       chemistry
+       Drawing
+       sub()
+    }
+
+}
+   
+console.log(person);
