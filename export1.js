f495366ceef471318d6369a940020807d83afa59
yaswanth9848 <yashwanth.sai.pavuluri@gmail.com>
2022-10-21 17:25:45 +0530

    import and export

--- export1.js
+++ export1.js
@@ -1,4 +1,4 @@
-const abc ={
+const person ={
 
     name:"Kishor",
     age:"21",
@@ -7,4 +7,30 @@ const abc ={
     company:"TCS"
 }
 
-exports.abc = abc;
+exports.person = person;
+
+const district = [
+    {name:"Guntur",special:"Mirchi"},
+    {name:"krishna",special:"KuchipudiDance"},
+    {name:"Nellore",special:"Beach"}
+
+]
+
+exports.district = district;
+
+const apps =["whatsapp","Facebook","Telegram","instagram"]
+exports.apps = apps;
+
+//4.cars
+const cars = [
+    {Name:"TATA",owner:"RatanTata"},
+    {Name:"suzuki",owner:"MarutiSuzuki"},
+    {Name:"Mahindra",owner:"AnandMahindra"},
+    {Name:"Toyota",owner:"AkioToyoda"}
+
+]
+exports.cars = cars;
+
+//5.bikes
+const bikes = ["hero","Honda","Tvs","Bajaj","Royal Enfield","ktm"];
+exports.bikes = bikes;
--- import1.js
+++ import1.js
@@ -1,2 +1,16 @@
 const def = require('./export1')
-console.log(def.abc.village);
+console.log(def.person.village);
+//2.importing districts
+const dis = require('./export1')
+console.log(dis.district);
+//3.Apps
+const app = require('./export1')
+console.log(app.apps);
+
+//4.cars
+const car = require('./export1')
+console.log(car.cars);
+
+//5.bikes
+const bike = require('./export1')
+console.log(bike.bikes);
